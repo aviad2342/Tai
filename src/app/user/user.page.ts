cf465bb0b30b53ba48e7beb19d5ef47b2fbcd451
Aviad <aviad2342@walla.com>
2021-02-02 15:24:26 +0200

    expandable

--- src/app/user/user.page.ts
+++ src/app/user/user.page.ts
@@ -1,9 +1,9 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Component, OnInit, OnDestroy, ViewChild } from '@angular/core';
 import { Subscription } from 'rxjs';
 import { Router } from '@angular/router';
 import { UserService } from './user.service';
 import { User } from './user.model';
-import { IonItemSliding } from '@ionic/angular';
+import { AlertController, IonItemSliding, IonList } from '@ionic/angular';
 import { AppService } from '../app.service';
 
 
@@ -16,16 +16,26 @@ import { AppService } from '../app.service';
 })
 export class UserPage implements OnInit, OnDestroy {
 
-  users: User[];
+  users: User[] = [];
+  @ViewChild('dynamicList') slidingList: IonList;
   private usersSubscription: Subscription;
   isDesktop: boolean;
+  isLoading = false;
 
-  constructor(private userService: UserService, private router: Router, private appService: AppService) { }
+  constructor(
+    private userService: UserService,
+    private router: Router,
+    private appService: AppService,
+    private alertController: AlertController
+    ) { }
 
   ngOnInit() {
+    this.isLoading = true;
     this.usersSubscription = this.userService.users.subscribe(users => {
       this.users = users;
+      this.isLoading = false;
     }, error => {
+      this.isLoading = false;
       console.log(error);
     });
   }
@@ -35,17 +45,43 @@ export class UserPage implements OnInit, OnDestroy {
     this.userService.getUsers().subscribe();
   }
 
-  onEdit(id: string, slidingItem: IonItemSliding) {
-    slidingItem.close();
+  async onEdit(id: string) {
+    await this.slidingList.closeSlidingItems();
     this.router.navigate(['tabs', 'user', 'edit', id]);
-    console.log('Editing item', id);
   }
 
-  onDelete(id: string, slidingItem: IonItemSliding) {
-    slidingItem.close();
-    this.userService.deleteUser(id).subscribe(() => {
-    });
+  async onDelete(id: string) {
+    await this.slidingList.closeSlidingItems();
+    this.onDeleteUser(id);
   }
+
+  async onDeleteUser(id: string) {
+    const alert = await this.alertController.create({
+      cssClass: 'delete-user-alert',
+      header: 'אישור מחיקת משתמש',
+      message: `האם אתה בטוח שברצונך למחוק את המשתמש לצמיתות?`,
+      mode: 'ios',
+      buttons: [
+        {
+          text: 'ביטול',
+          role: 'cancel',
+          cssClass: 'delete-user-alert-btn-cancel',
+          handler: () => {
+          }
+        }, {
+          text: 'אישור',
+          handler: () => {
+            this.userService.deleteUser(id).subscribe( () => {
+              this.appService.presentToast('המשתמש נשמר בהצלחה', true);
+            }, error => {
+              this.appService.presentToast('חלה תקלה לא ניתן למחוק את המשתמש כעת!', false);
+            });
+          }
+        }
+      ]
+    });
+    await alert.present();
+}
 
   ngOnDestroy() {
     if (this.usersSubscription) {
