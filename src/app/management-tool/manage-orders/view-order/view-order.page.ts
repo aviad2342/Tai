a7653e36f6ec2922643ce0d80d9d8fc8ecb76ccd
Aviad <aviad2342@walla.com>
2021-04-14 15:41:51 +0300

    fonts

--- src/app/management-tool/manage-orders/view-order/view-order.page.ts
+++ src/app/management-tool/manage-orders/view-order/view-order.page.ts
@@ -10,6 +10,11 @@ import { jsPDF } from 'jspdf';
 // import * as jspdf from 'jspdf';
 // import * as html2canvas from 'html2canvas';
 // import * as html2canvas from 'html2canvas';
+import * as pdfMake from 'pdfmake/build/pdfmake';
+import * as pdfFonts from 'pdfmake/build/vfs_fonts';
+import { TDocumentDefinitions } from 'pdfmake/interfaces';
+(pdfMake as any).vfs = pdfFonts.pdfMake.vfs;
+
 
 @Component({
   selector: 'app-view-order',
@@ -23,7 +28,12 @@ export class ViewOrderPage implements OnInit {
   isLoading = false;
   useCoupon = false;
   activeUrl = '';
-  @ViewChild('invoice') container: ElementRef;
+  letterObj = {
+    to: '',
+    from: '',
+    text: ''
+  }
+  pdfObj = null;
 
   constructor(
     private route: ActivatedRoute,
@@ -76,6 +86,68 @@ export class ViewOrderPage implements OnInit {
     });
   }
 
+  createPdf() {
+    this.letterObj.from = 'bla';
+    this.letterObj.to = this?.order.user.firstName + ' ' + this?.order.user.lastName;
+    this.letterObj.text = 'dvvsdv gergeg ht htrhrth hrthrth rthrthrth rth';
+    const fonts = (pdfMake as any).fonts = {
+      Yoav: {
+        normal: 'Yoav.TTF',
+        bold: 'Yoav-bold.TTF',
+      },
+      Roboto: {
+        normal: 'https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.66/fonts/Roboto/Roboto-Regular.ttf',
+        bold: 'https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.66/fonts/Roboto/Roboto-Medium.ttf',
+        italics: 'https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.66/fonts/Roboto/Roboto-Italic.ttf',
+        bolditalics: 'https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.66/fonts/Roboto/Roboto-MediumItalic.ttf'
+      }
+   }
+    const docDefinition: TDocumentDefinitions = {
+      content: [
+        { text: 'REMINDER', style: 'header' },
+        { text: new Date().toUTCString(), alignment: 'right' },
+
+        { text: 'From', style: 'subheader' },
+        { text: this.letterObj.from },
+
+        { text: 'To', style: 'subheader' },
+        this.letterObj.to,
+
+        { text: this.letterObj.text, style: 'story', margin: [0, 20, 0, 20] },
+
+        {
+          ul: [
+            'Bacon',
+            'Rips',
+            'BBQ',
+          ]
+        }
+      ],
+      styles: {
+        with: {},
+        header: {
+          fontSize: 18,
+          bold: true,
+        },
+        subheader: {
+          font: 'Yoav',
+          fontSize: 14,
+          bold: false,
+          margin: [0, 15, 0, 0]
+        },
+        story: {
+          italics: true,
+          alignment: 'center'
+        }
+      }
+    }
+    this.pdfObj = pdfMake.createPdf(docDefinition, null, fonts);
+  }
+
+  downloadPdf() {
+    this.pdfObj.download();
+  }
+
   // generateInvoicePdf() {
   //   const doc = new jsPDF('p', 'mm', 'a4');
   //   doc.setR2L(true);
@@ -88,20 +160,24 @@ export class ViewOrderPage implements OnInit {
   //   doc.save('a4.pdf');
   // }
 
-  convertToPdf(){
-    // const data = document.getElementById('print-container');
+  convertToPdf() {
+    const data = document.getElementById('print-container');
+    const pdf = new jsPDF('p', 'mm', 'a4'); // A4 size page of PDF
+      pdf.html(data);
+      pdf.save('report.pdf'); // Generated PDF
     // html2canvas(data).then((canvas) => {
     //   // Few necessary setting options
-    //   const imgWidth = 208;
-    //   const pageHeight = 295;
-    //   const imgHeight = canvas.height * imgWidth / canvas.width;
-    //   const heightLeft = imgHeight;
+    //   // const imgWidth = 208;
+    //   // const pageHeight = 295;
+    //   // const imgHeight = canvas.height * imgWidth / canvas.width;
+    //   // const heightLeft = imgHeight;
 
-    //   const contentDataURL = canvas.toDataURL('image/png')
+    //   // const contentDataURL = canvas.toDataURL('image/png')
     //   const pdf = new jsPDF('p', 'mm', 'a4'); // A4 size page of PDF
-    //   const position = 0;
-    //   pdf.addImage(contentDataURL, 'PNG', 0, position, imgWidth, imgHeight)
-
+    //   // const position = 0;
+    //   // pdf.addImage(contentDataURL, 'PNG', 0, position, imgWidth, imgHeight)
+    //   // pdf.html(canvas);
+    //   pdf.text('canvas', 100, 10, {align:'center'});
     //   pdf.save('report.pdf'); // Generated PDF
     //       });
 
