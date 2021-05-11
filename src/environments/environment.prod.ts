9021613f9a51b65a9e1728093434fdbe304b7937
Aviad <aviad2342@walla.com>
2021-05-11 21:25:53 +0300

    production

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  googleAPIKey: 'AIzaSyDqb_--ZW9Sn4l75YuinoYD2Fgeu6gQkGY',
+  YouTubeAPIKey: 'AIzaSyDJmTLN9-yieVrbAE91GXKpGxICkRIejwQ',
+  LOCALHOST: 'aviadbenhayun.com' // 'localhost'
 };
